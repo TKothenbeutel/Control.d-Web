5ddb0f4b5e322e2d8629d3d8daca91b2092e2cb4
Taylor Kothenbeutel <166166061+TKothenbeutel@users.noreply.github.com>
2026-06-04 21:36:40 -0500

    Project Initialized

---
+++ src/main/java/com/controld/controld/ControldApplication.java
@@ -0,0 +1,13 @@
+package com.controld.controld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControldApplication.class, args);
+	}
+
+}
